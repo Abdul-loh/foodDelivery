ccf8f74f0890ead8fae7bb7e0c5ff7e1189341fc
abdul-loh <abdullahiabdulkereem0@gmail.com>
2024-12-07 06:09:40 -0800

    Declare of the value

--- foodOrder/src/Pages/Cart/Cart.tsx
+++ foodOrder/src/Pages/Cart/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import "./Cart.css";
 import { StoreMenu } from "../../Context/StoreMenu";
 import { useNavigate } from "react-router-dom";
